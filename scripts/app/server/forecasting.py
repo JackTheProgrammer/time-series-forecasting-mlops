d464fb90bcd56b875acba7db909848c738f9a03b
JackTheProgrammer <45397794+JackTheProgrammer@users.noreply.github.com>
2026-04-29 18:39:24 +0500

    created forecasting dataloader which picks stocks of gold from 2020 till the current datetime

--- scripts/app/server/forecasting.py
+++ scripts/app/server/forecasting.py
@@ -1,17 +1,7 @@
 from yfinance import download
 from datetime import datetime
-from torch import tensor, float32, cuda
+from torch import tensor, float32
 from torch.utils.data import Dataset, DataLoader
-
-
-def preprocess_stock_price_data():
-    todays_gold_stock_price = download("GC=F", start=datetime(2010, 1, 1), end=datetime.now())
-    stock_price_data = todays_gold_stock_price[["Close"]]
-    stock_price_data.drop([0,1], inplace=True)
-    stock_price_data.rename(columns={'Price': 'Date', 'Close': 'closing_price'}, inplace=True)
-    stock_price_data.set_index('Date', inplace=True)
-    stock_price_data.index = stock_price_data.index.astype('datetime64[ns]')
-    return stock_price_data
 
 # I intend to make a dataset for forecasting purpose, this 
 # means that there're no X, y based sequences creation
@@ -27,11 +17,8 @@ class GoldStockPriceDataset(Dataset):
         stock_price_values = self.stock_prices['Close'].values
         return tensor(stock_price_values[idx:idx + self.window_size], dtype=float32)
 
-todays_gold_stock_price = preprocess_stock_price_data()
+todays_gold_stock_price = download("GC=F", start='2020-01-01', end=datetime.now().strftime("%Y-%m-%d"))
+todays_gold_stock_price = todays_gold_stock_price[["Close"]]
+
 gold_stock_dataset = GoldStockPriceDataset(todays_gold_stock_price, window_size=30)
 gold_stock_dataloader = DataLoader(gold_stock_dataset, batch_size=32, shuffle=True)
-
-# testing the dataloader
-for batch in gold_stock_dataloader:
-    print(batch)
-    break
