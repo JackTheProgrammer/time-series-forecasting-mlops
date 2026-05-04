b57577fb9f55d4d83641e4f1a6622b9c6a4d73a8
JackTheProgrammer <45397794+JackTheProgrammer@users.noreply.github.com>
2026-05-04 16:34:49 +0500

    added flexibility for managing the directories non-existence

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -1,11 +1,25 @@
 from pandas import read_csv, to_datetime
 from yfinance import download
 from datetime import datetime
+from pathlib import Path
+
+data_dir = 'data'
+raw_data_dir = f"{data_dir}/raw"
+processed_data_dir = f"{data_dir}/processed"
+
+data_path = Path(data_dir)
+data_path.mkdir(exist_ok=True)
+
+raw_data_path = Path(raw_data_dir)
+raw_data_path.mkdir(exist_ok=True)
+
+processed_data_path = Path(processed_data_dir)
+processed_data_path.mkdir(exist_ok=True)
 
 data = download("GC=F", start='2010-01-01', end=datetime.now().strftime("%Y-%m-%d"))
 data = data[["Close"]]
 
-raw_data_path = f"data/raw/gold_price_data_{datetime.today().strftime('%Y-%m-%d')}.csv"
+raw_data_path = f"{raw_data_dir}/gold_price_data_{datetime.today().strftime('%Y-%m-%d')}.csv"
 data.to_csv(raw_data_path, index=True)
 
 goldstock_raw_data = read_csv(raw_data_path)
@@ -23,5 +37,5 @@ type_mapping = {
 }
 goldstock_raw_data = goldstock_raw_data.astype(type_mapping)
 
-preprocessed_data_path = f"data/processed/gold_data_preprocessed_{datetime.now().strftime('%Y-%m-%d')}.csv"
+preprocessed_data_path = f"{processed_data_dir}/gold_data_preprocessed_{datetime.now().strftime('%Y-%m-%d')}.csv"
 goldstock_raw_data.to_csv(preprocessed_data_path, index_label='Date')
